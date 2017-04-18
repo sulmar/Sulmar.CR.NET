6091addacc5afd9e6323edad528fc67eeabbef7f
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-04-18 20:14:06 +0200

    Dodano aplikacji WPF

---
+++ Sulmar.CR.NET.WPFClient/ReportWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sulmar.CR.NET.WPFClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var rpt = new ReportDocument();
+            rpt.Load(@"Reports\JedenWierszArtykulParagraf.rpt");
+
+            if (rpt.IsLoaded)
+            {
+                CrystalReportsViewer1.ViewerCore.ReportSource = rpt;
+            }
+        }
+    }
+}
---
+++ Sulmar.CR.NET.WPFClient/ViewModels/ReportViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sulmar.CR.NET.WPFClient.ViewModels
+{
+    public class ReportViewModel
+    {
+        public ReportDocument Report { get; set; }
+
+        public ReportViewModel()
+        {
+            LoadReport();
+
+        }
+
+        private void LoadReport()
+        {
+            var rpt = new ReportDocument();
+            rpt.Load(@"Reports\JedenWierszArtykulParagraf.rpt");
+
+            Report = rpt;
+        }
+    }
+}
